eeff48b08777e530ea03ca24e6064acc0486f400
lmonsalve <lmonsalve22@gmail.com>
2020-03-10 21:29:37 -0300

    commit

---
+++ DjangoWebProject6/DjangoWebProject6/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\limc_\source\repos\DjangoWebProject6\DjangoWebProject6\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
